d7b34579e89973621378841f228d457bd1d1984e
Mark Hernandez <m.hern1415@gmail.com>
2019-09-06 18:59:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,6 +7,17 @@ def initialize(attributes)
   self.id ||= nil
 end
   
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+  end
   
   
   
