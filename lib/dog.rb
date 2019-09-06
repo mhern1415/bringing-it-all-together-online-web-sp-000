d2fc122039d91ef52efaa4ae8fb426969832a3ea
Mark Hernandez <m.hern1415@gmail.com>
2019-09-06 18:58:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   
 attr_accessor :id, :name, :breed, 
 
-def initialize 
+def initialize(attributes)
   attributes.each {|key, value| self.send(("#{key}="), value)}
   self.id ||= nil
 end
