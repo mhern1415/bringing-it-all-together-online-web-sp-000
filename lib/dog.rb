cf0af2ca45263644db09823ec8a83e11379de012
Mark Hernandez <m.hern1415@gmail.com>
2019-09-06 19:47:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -60,5 +60,23 @@ def self.find_by_id(id)
     self.new_from_db(row)
   end.first
 end
+
+  def self.find_or_create_by(name:, breed:)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ? AND breed = ?
+      SQL
+
+
+      dog = DB[:conn].execute(sql, name, breed).first
+
+      if dog
+        new_dog = self.new_from_db(dog)
+      else
+        new_dog = self.create({:name => name, :breed => breed})
+      end
+      new_dog
+  end
+  
   
 end
